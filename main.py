1051657c5d040557ca33f10e3e906aec2f8b367b
NaruKodland <ycami.kodland@gmail.com>
2025-02-21 20:18:25 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+#Importar el random
+caracteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+#Solicitar longitud de la contraseña al usuario
+password = ""
+
+#bucle for con valor del range según lo digitado por el usuario
+    #Generar un valor al azar de los caracteres
+    #Concatenar el caracter al azar según
+#imprimir contraseña generada
